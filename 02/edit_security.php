054c3b2f4a121c575d1ddc07ca27c20549a076d7
Odilbukh <odil.bukh@mail.ru>
2021-03-19 13:43:11 +0500

    changing 08.02

--- 02/edit_security.php
+++ 02/edit_security.php
@@ -2,29 +2,6 @@
 session_start();
 require_once 'functions.php';
 
-//$email = $_POST['email'];
-//$password = $_POST['password'];
-//$id = $_SESSION['user_data']['id'];
-//
-//$my_email = $_SESSION['log-in']['email'];
-//
-//$user = get_user_by_email($email);
-//$check_email = is_my_email($my_email, $email);
-//
-//if (($user['email'] !== $email) && $check_email == false )
-//{
-//    set_flash_message('danger', 'Пользователь с этим e-mail уже существует');
-//    redirect_to('security.php');
-//}
-//else
-//{
-//    edit_credentials($email, $password, $id);
-//
-//    set_flash_message('success', 'Профиль успешно обновлен !');
-//    redirect_to('users.php');
-//}
-
-
 $email = $_POST["email"];
 $password = $_POST["password"];
 $id = $_SESSION['user_data']['id'];
