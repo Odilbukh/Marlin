fc994b8081c8003f388d735ba247e08d26771125
Odilbukh <odil.bukh@mail.ru>
2021-04-06 17:13:47 +0500

    404

---
+++ Diplom/includes/404.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>Упс, произошла ошибка. Страница не найдена.</h1>
+
+<h2>Вернутся на <a href="../index.php">главное страницу</a></h2>
+</body>
+</html>
