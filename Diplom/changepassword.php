379298205f7ecf0b2cfe923bc10acdf0f3a1fd01
Odilbukh <odil.bukh@mail.ru>
2021-04-07 14:17:30 +0500

    Update changepassword.php

--- Diplom/changepassword.php
+++ Diplom/changepassword.php
@@ -17,7 +17,7 @@ if (Input::exists()) {
             if (password_verify(Input::get('current_password'), $user->data()->password)) {
                 $user->update(['password' => password_hash(Input::get('new_password'), PASSWORD_DEFAULT)]);
                 Session::flash('success', 'Password changed successfully');
-                Redirect::to('index.php');
+                Redirect::to('changepassword.php');
             }
         } else {
             $danger = '';
